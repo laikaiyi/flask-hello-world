cf22ec6277f3c84801197fd2a27b73a7a6a4f732
laikaiyi <163814074+laikaiyi@users.noreply.github.com>
2024-03-21 01:19:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,10 +26,7 @@ def linebot():
         line_bot_api = LineBotApi('FgRwDsmCC055uMWW/YTiv/X/+vP+WzYMz/X6nl9wde6iaB/Q/C1chDgrGCDxHJVnK5mcR8wJmKuYCT5cK5OGRbw1e6BW9C9JtQaQefu7hJ9vQ8WqomLTg4kjzZDYxNk4fIQmma6GJMZAKRauBWTjQgdB04t89/1O/w1cDnyilFU=')
         handler = WebhookHandler('d59461ed36fad764c35e258a67fe724f')
         signature = request.headers['X-Line-Signature']
-        handler.handle(body, signature)
         tk = json_data['events'][0]['replyToken']                 # 取得 reply token
-        msg = json_data['events'][0]['message']['text']           # 取得使用者發送的訊息
-        signature = request.headers['X-Line-Signature']
         handler.handle(body, signature)
         user_id = json_data['events'][0]['source']['userId']  # 取得使用者 ID
         msg = json_data['events'][0]['message']['text']   # 取得使用者發送的訊息
